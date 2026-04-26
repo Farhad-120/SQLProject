8edaa183fb63663b91b8e16c4a9e8b4a4bdf229f
MD Farhad Hossain <Farhadhossain25000@gmail.com>
2026-04-27 00:51:29 +0600

    Update Store procedurep2.sql

--- Store procedurep2.sql
+++ Store procedurep2.sql
@@ -97,3 +97,51 @@ EXEC UpdateStudents
 	 @Age =15;
 
   EXEC UpdateStudents @Name = 'Alices', @RollNo = 101, @Age = 20;
+
+  Go
+
+  ------ Store Procedure  data delete koro---------------
+
+
+  CREATE PROC DeleteStudents 
+              @Name Varchar(30)
+
+
+	AS
+	BEGIN
+		DELETE FROM Students 
+		Where Name=  @Name ;
+
+
+		PRINT 'Student Deleted Successfully'
+	END;
+
+	 EXEC DeleteStudents @Name='Ramim'
+
+
+	 ----------------------------Condition dya Delete koro store procedure-------------------
+	 Go
+
+	 CREATE PROC DeleteStudentsId
+	             @StudentId Int
+
+	AS
+	BEGIN
+		IF EXISTS( SELECT 1 FROM Students where StudentId=@StudentId)
+			BEGIN
+				Delete From Students
+				where StudentId=@StudentId
+
+				PRINT 'Student Data deleted successfully!!'
+			END
+
+
+		ELSE
+
+			BEGIN
+			   PRINT 'Student not deleted!'
+			END
+
+	END;
+
+		 EXEC DeleteStudentsId @StudentId=2
